28bfbbd1d29f0bf49da13e4582e084df82373e48
Logan Donovan <commit@logandonovan.com>
2017-10-05 17:35:08 -0400

    update quote generator to respond to POST and other types of requests to /quote separately from GET

--- lib/quote_generator.rb
+++ lib/quote_generator.rb
@@ -7,8 +7,14 @@ class QuoteGenerator
     req = Rack::Request.new(env)
     case req.path_info
     when /quote/
-      gen = Random.new()
-      [200, {"Content-Type" => "text/plain"}, [GERVAIS_QUOTES[gen.rand(0...GERVAIS_QUOTES.length)]]]
+      if req.post?
+        [404, {"Content-Type" => "text/plain"}, []]
+      elsif req.get?
+        gen = Random.new()
+        [200, {"Content-Type" => "text/plain"}, [GERVAIS_QUOTES[gen.rand(0...GERVAIS_QUOTES.length)]]]
+      else
+        [404, {"Content-Type" => "text/plain"}, ["These aren't the request types you're looking for. Try a GET."]]
+      end
     else
       @app.call(env)
     end
