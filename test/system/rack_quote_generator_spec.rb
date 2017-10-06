95ad0b8ed874971ee8fabe9f1580be924cb93282
Logan Donovan <commit@logandonovan.com>
2017-10-05 22:22:13 -0400

    consolidate middleware instance in quote generator tests spec

--- test/system/rack_quote_generator_spec.rb
+++ test/system/rack_quote_generator_spec.rb
@@ -8,31 +8,32 @@ RSpec.describe QuoteGenerator do
   let(:app) { proc { default_app } }
   let(:env) { double(:env) }
 
+  let(:middleware) { QuoteGenerator.new(app) }
+
   it 'returns a quote on GET /quote' do
-    middleware = QuoteGenerator.new(app)
     env = Rack::MockRequest.env_for("/quote")
     status, _, body = middleware.call(env)
     expect(status).to eq(200)
     expect(GERVAIS_TEST_QUOTES).to include(body[0])
   end
+
   it 'returns an error with empty body on POST /quote' do
-    middleware = QuoteGenerator.new(app)
     env = Rack::MockRequest.env_for("/quote",
       "REQUEST_METHOD" => 'POST')
     status, _, body = middleware.call(env)
     expect(status).to eq(404)
     expect(nil).to eq(body[0])
   end
+
   it 'returns humor on any other request type on /quote' do
-    middleware = QuoteGenerator.new(app)
     env = Rack::MockRequest.env_for("/quote",
       "REQUEST_METHOD" => 'DELETE')
     status, _, body = middleware.call(env)
     expect(status).to eq(404)
     expect(body).to eq(["These aren't the request types you're looking for. Try a GET."])
   end
+
   it 'returns the app response on anything else' do
-    middleware = QuoteGenerator.new(app)
     env = Rack::MockRequest.env_for("/")
     status, _, body = middleware.call(env)
     expect(status).to eq(200)
