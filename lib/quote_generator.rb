a94716049939c70b675d71cbd53bd1c98de327c2
Logan Donovan <commit@logandonovan.com>
2017-10-08 17:44:45 -0400

    cleanup QuoteGenerator to optimize first call performance and simplify functions

--- lib/quote_generator.rb
+++ lib/quote_generator.rb
@@ -4,22 +4,23 @@ MERCHANT_QUOTES = File.readlines('data/merchant_quotes.txt').map{ |line| line.st
 class QuoteGenerator
   def initialize(app)
     @app = app
-    @quote_index = Queue.new
     @quotes = GERVAIS_QUOTES + MERCHANT_QUOTES
+    generate_quote_index
+    @all_quotes_range = get_randomized_array_of_quote_indexes
   end
 
   def call(env)
     req = Rack::Request.new(env)
     case req.path_info
     when /all-quotes/
-      quote_index = get_randomized_array_of_quote_indexes
-      quotes = quote_index.map { |i, index| @quotes[i] + "\n" }.join("")
+      @all_quotes_range.shuffle()
+      quotes = @all_quotes_range.map { |i, index| @quotes[i] + "\n" }.join("")
       [200, {"Content-Type" => "text/plain"}, [quotes]]
     when /quote/
       if req.post?
         [404, {"Content-Type" => "text/plain"}, []]
       elsif req.get?
-        @quote_index = generate_quote_index if @quote_index.length == 0
+        generate_quote_index if @quote_index.length == 0
         [200, {"Content-Type" => "text/plain"}, [@quotes[@quote_index.pop()]]]
       else
         [404, {"Content-Type" => "text/plain"}, ["These aren't the request types you're looking for. Try a GET."]]
@@ -30,13 +31,11 @@ class QuoteGenerator
   end
 
   def generate_quote_index
-    @quote_index.clear()
-    range_array = get_randomized_array_of_quote_indexes
-    range_array.each { |elem| @quote_index.push(elem) }
+    @quote_index ||= Queue.new
+    get_randomized_array_of_quote_indexes.each { |elem| @quote_index.push(elem) }
   end
 
   def get_randomized_array_of_quote_indexes
-    range = 0...@quotes.length
-    range.to_a.shuffle()
+    (0...@quotes.length).to_a.shuffle()
   end
 end
